7ea6059541cf258fe4c749f204063b144fdedf5b
Marcus Vinicius Pizzo Pizzo Cognolatto Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-05-06 12:11:56 -0300

    EP 1
    Primeira versão do EP I

---
+++ Programas/EP1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int n50, n20, n5, n1, valor;
+int main() {
+    printf("Digite um valor inteiro a ser retirado: R$\n");
+    scanf("%i", &valor);
+    n50 = valor/50;
+    printf("O número de notas de 50 reais a serem emitidas são %i\n", n50);
+    n20 = (valor-n50*50)/20;
+    printf("O número de notas de 20 reais a serem emitidas são %i\n", n20);
+    n5 = (valor-n50*50 - n20*20)/5;
+    printf("O número de notas de 5 reais a serem emitidas são %i\n", n5);
+    n1 = (valor-n50*50 -n20*20 -n5*5)/1;
+    printf("O número de notas de 1 real a serem emitidas é %i\n", n1);
+    return 0;
+
+}
