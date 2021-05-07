86120c1f392750e0e8e84d57733689b4ede03b42
Marcus Vinicius Pizzo Pizzo Cognolatto Santos <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-05-07 13:28:04 -0300

    1v
    primeira versão

--- Programas/teste.c
+++ Programas/teste.c
@@ -3,7 +3,30 @@ Este programa serve para calcular a área de uma casa e sua piscina
 */
 #include <stdio.h>
 #include <math.h>
+float lateral, comprimento, areas, areaq, areap, areat;
+double raio;
 
-void areacasa() {
-    
+void areainterna(float lateral, comprimento){
+    areas = pow(lateral,2);
+    printf("Area da sala: %f\n", areas);
+    areaq = (lateral/2)*comprimento;
+    printf("Area do quarto: %f\n", areaq);
+    printf("Area do banheiro: %f\n", areaq);
+    return 0;
+}
+double areapiscina(double raio) {
+    return (M_PI*pow(raio,2));
+}
+int main() {
+printf("Digite o valor da lateral da casa:\n");
+scanf("f", &lateral);
+printf("Digite o valor do comprimento da casa:\n");
+scanf("f", &comprimento);
+printf("Digite o valor do raio da piscina:\n");
+scanf("d", &raio);
+areainterna()
+areapscina()
+areat = areas + 2*areaq + areap + areat;
+printf("Area total: %f\n", areat);
+return 0;
 }
