afe74bf101cc78fb831a611fdf600f81415709e9
Marcus-vinicius-pcs <83514936+Marcus-vinicius-pcs@users.noreply.github.com>
2021-08-17 18:06:32 -0300

    Atividade semanal 1

---
+++ Programas/atividades semanais/atv-sem-1/atv1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+ //EXERCICIO 1
+
+    int nusp = 12543478;
+    printf("Imprimindo inteiro: %i\n", nusp);
+    printf("Imprimindo numero: %d\n", nusp);
+    printf("Imprimindo float (com cast): %f\n", (float)nusp);
+    printf("Imprimindo quociente: %i\n", nusp/2);
+    printf("Imprimindo resto: %d\n", nusp%2);
+    printf("Imprimindo quadrado: %d\n", nusp*nusp);
+    return 0;
+
+//EXERCICIO 2
+    int x = 7;
+    int y = 13;
+    int* z = &x;
+    printf("x: %i   y: %i   z: %p\n", x, y, z);
+    printf("&x: %p   &y: %p   &z: %p\n", &x, &y, &z);
+    *z = x + y;
+    printf("Novo valor de x: %i\n", x);    
+    return 0;
+}
+}
